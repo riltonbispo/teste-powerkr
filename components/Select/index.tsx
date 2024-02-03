1f8c5f0e9f47d93f999a43477688ec80b14a2786
riltonbispo <riltondev@gmail.com>
2024-02-03 10:11:36 -0300

    feat: select component

---
+++ components/Select/index.tsx
@@ -0,0 +1,118 @@
+import { ChevronDown, ChevronUp, Check } from '@tamagui/lucide-icons'
+import { useState, useMemo } from 'react'
+import { SelectProps, Select, Adapt, Sheet, YStack, getFontSize, FontSizeTokens } from 'tamagui'
+
+export function SelectDemoItem(props: SelectProps) {
+  const [val, setVal] = useState('todos')
+
+
+  const items = [
+    { name: 'todos' },
+    { name: 'alguns' },
+    { name: 'nenhum' }
+  ]
+
+  return (
+    <Select value={val} onValueChange={setVal} disablePreventBodyScroll {...props} >
+      <Select.Trigger width={100} iconAfter={ChevronDown} theme={'light'}>
+        <Select.Value placeholder="Something" />
+      </Select.Trigger>
+
+      <Adapt when="sm" platform="touch">
+        <Sheet
+          native={!!props.native}
+          modal
+          dismissOnSnapToBottom
+          animationConfig={{
+            type: 'spring',
+            damping: 20,
+            mass: 1.2,
+            stiffness: 250,
+          }}
+        >
+          <Sheet.Frame>
+            <Sheet.ScrollView>
+              <Adapt.Contents />
+            </Sheet.ScrollView>
+          </Sheet.Frame>
+          <Sheet.Overlay
+            animation="lazy"
+            enterStyle={{ opacity: 0 }}
+            exitStyle={{ opacity: 0 }}
+          />
+        </Sheet>
+      </Adapt>
+
+      <Select.Content zIndex={200000}>
+        <Select.ScrollUpButton
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          width="$8"
+          height="$3"
+        >
+          <YStack zIndex={10}>
+            <ChevronUp size={20} />
+          </YStack>
+        </Select.ScrollUpButton>
+
+        <Select.Viewport minWidth={200}>
+          <Select.Group
+          >
+            <Select.Label
+            >Filtro</Select.Label>
+            {/* for longer lists memoizing these is useful */}
+            {useMemo(
+              () =>
+                items.map((item, i) => {
+                  return (
+                    <Select.Item
+                      index={i}
+                      key={item.name}
+                      value={item.name.toLowerCase()}
+                    >
+                      <Select.ItemText>{item.name}</Select.ItemText>
+                      <Select.ItemIndicator marginLeft="auto">
+                        <Check size={16} />
+                      </Select.ItemIndicator>
+                    </Select.Item>
+                  )
+                }),
+              [items]
+            )}
+          </Select.Group>
+          {/* Native gets an extra icon */}
+          {props.native && (
+            <YStack
+              position="absolute"
+              right={0}
+              top={0}
+              bottom={0}
+              alignItems="center"
+              justifyContent="center"
+              width={'$4'}
+              pointerEvents="none"
+            >
+              <ChevronDown
+                size={getFontSize((props.size as FontSizeTokens) ?? '$true')}
+              />
+            </YStack>
+          )}
+        </Select.Viewport>
+
+        <Select.ScrollDownButton
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+          width="100%"
+          height="$3"
+        >
+          <YStack zIndex={10}>
+            <ChevronDown size={20} />
+          </YStack>
+
+        </Select.ScrollDownButton>
+      </Select.Content>
+    </Select>
+  )
+}
